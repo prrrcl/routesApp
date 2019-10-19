5bd0c3045e6d507ee7da6fb9a3119bbecc3b175a
prrrcl <adrianporcelnavarro@hotmail.com>
2019-10-19 17:53:37 +0200

    Added routes in app.js and some config in json files

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import SearcherScreen from './screens/Searcher'
+import ListScreen from './screens/List'
+import DetailScreen from './screens/Detail'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const AppNavigator = createStackNavigator({
+  Searcher: {
+    screen: SearcherScreen
   },
-});
+  List: {
+    screen: ListScreen
+  },
+  Detail: {
+    screen: DetailScreen
+  }
+},
+  {
+    initialRouteName: 'Searcher'
+})
+
+export default createAppContainer(AppNavigator)
