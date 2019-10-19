2412d9d28349f67fd5f21a98b7390a2bf87f1dda
prrrcl <adrianporcelnavarro@hotmail.com>
2019-10-19 17:53:03 +0200

    Added listitem

---
+++ components/ListItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const ListItem = ({name, from, to, onPress}) => {
+  return(
+    <View>
+      <Text>Hola</Text>
+    </View>
+  )
+}
+
+export default ListItem
